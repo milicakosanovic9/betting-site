86f07a4c811ebbc8fc50972e12327309fb0627c3
milicakosanovic <milicakosanovic199@gmail.com>
2024-07-10 11:00:26 +0200

    Sport betting site.

---
+++ Milica_Kosanovic_IT69_21/js/functions.js
@@ -0,0 +1,19 @@
+$(".dodaci .fa-bars").click(function(){
+	$("#hamburger-menu").slideToggle(300);
+});
+
+$("#hamburger-menu .li-hamb-menu-1").hover(function(){
+	$(".hamb-drop-menu-1").slideToggle(300);
+});
+
+$("#hamburger-menu .li-hamb-menu-2").hover(function(){
+	$(".hamb-drop-menu-2").slideToggle(300);
+});
+
+$("#hamburger-menu .li-hamb-menu-3").hover(function(){
+	$(".hamb-drop-menu-3").slideToggle(300);
+});
+
+$("#hamburger-menu .li-hamb-menu-4").hover(function(){
+	$(".hamb-drop-menu-4").slideToggle(300);
+});
